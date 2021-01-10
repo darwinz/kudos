281339860d274e5915e9021a95494ded5a16742e
Brandon Johnson <brandon@moov.io>
2021-01-10 00:50:53 -0700

    Update App.vue, add Footer, Home, and Login components

---
+++ pkg/http/web/app/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+ <v-footer class="pa-3 white--text" color="teal" absolute>
+   <div>
+     Developed with ❤️  by {{YOUR_NAME}} &copy; {{ new Date().getFullYear() }}
+   </div>
+ </v-footer>
+</template>
