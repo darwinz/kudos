669c305560b0725b27d25ab2417ea0d3b03e19b8
Brandon Johnson <brandon@moov.io>
2021-01-10 00:56:48 -0700

    Add apiClient.js and githubClient.js

---
+++ pkg/http/web/app/src/apiClient.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+const BASE_URI = 'http://localhost:4444';
+const client = axios.create({
+  baseURL: BASE_URI,
+  json: true
+});
+
+const APIClient =  {
+  createKudo(repo) {
+    return this.perform('post', '/kudos', repo);
+  },
+
+  deleteKudo(repo) {
+    return this.perform('delete', `/kudos/${repo.id}`);
+  },
+
+  updateKudo(repo) {
+    return this.perform('put', `/kudos/${repo.id}`, repo);
+  },
+
+  getKudos() {
+    return this.perform('get', '/kudos');
+  },
+
+  getKudo(repo) {
+    return this.perform('get', `/kudo/${repo.id}`);
+  },
+
+  async perform (method, resource, data) {
+    let accessToken = await Vue.prototype.$auth.getAccessToken()
+    return client({
+      method,
+      url: resource,
+      data,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(req => {
+      return req.data
+    })
+  }
+}
+
+export default APIClient;
